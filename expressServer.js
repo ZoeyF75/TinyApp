dd281e468484631e196ddd81b1ac2424921aac81
Zoey Farago <zf.zoodle@gmail.com>
2021-01-19 00:25:07 +0000

    URL shortening, implementing new url

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  let ranNum = Math.floor(Math.random()*9);
+  //let ranLet = Math.random().toString(36);
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,27 +15,28 @@ const urlDatabase = {
 //tells express app to use ejs as its templating engine
 app.set("view engine", "ejs");
 
-//homepage
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+//converts the request body from a Buffer into string, adds the data to the req object under the key body
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+//get routes
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urlsIndex", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urlsNew");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
 res.render("urlsShow" , templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok'
 });
 
 app.listen(PORT, () => {

39deeeb3f070db5a4a2bacfd8bea927495cb1a4e
Zoey Farago <zf.zoodle@gmail.com>
2021-01-22 06:47:04 +0000

    Organizing code

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const bcrypt = require('bcrypt');
-const findEmail = require('./helper');
+const functions = require('./helper');
+const findEmail = functions.findEmail;
+const generateRandomString = functions.generateRandomString;
+const urlsForUser = functions.urlsForUser;
 //converts the request body from a Buffer into string, adds the data to the req object under the key body
 const bodyParser = require("body-parser");
 const cookieSession = require('cookie-session');
-
 const app = express();
 const PORT = 8080; // default port 8080
+const users = {}
+const urlDatabase = {};
 
 //tells express app to use ejs as its templating engine
 app.set("view engine", "ejs");
@@ -15,58 +19,50 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cookieSession({
   name: 'session',
-  keys: ['123banana']
+  keys: ['123banana'] //characters the cookie name is encrypted with
 }));
-
-const users = {}
-const urlDatabase = {};
-
-function generateRandomString() {
-  let alphanumeric = '';
-  for (let i = 0; i < 3; i++) {
-  alphanumeric += Math.floor(Math.random()*9);
-  alphanumeric += Math.random().toString(36).slice(2,3);  
-  }
-  return alphanumeric;
-};
-
-
-function urlsForUser(id) {
-  const userBase = {};
-  for(let u in urlDatabase) {
-    if (urlDatabase[u].userID === id) { //if database at the shortURL of the userID key equals id
-      userBase[u] = urlDatabase[u]; //adds key 
-    }
-  }
-  return userBase;
-};
 
 //routes below
 app.get("/register", (req, res) => {
-  const templateVars = { user: users[req.session.userID], users };
+  const templateVars = { 
+    user: users[req.session.userID] 
+  };
   return res.render('urlsRegistration.ejs', templateVars);
-
 });
 
 app.get("/login", (req, res) => {
-  const templateVars = { user: users[req.session.userID] };
+  const templateVars = { 
+    user: users[req.session.userID] 
+  };
   return res.render('urlsLogin.ejs', templateVars);
 });
 
 //My URLS page
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlsForUser(req.session.userID), user: users[req.session.userID], users };
+  const templateVars = { 
+    urls: urlsForUser(req.session.userID, urlDatabase), 
+    user: users[req.session.userID]
+   };
   return res.render('urlsIndex', templateVars);
 });
+
 //create new shortURL route
 app.get("/urls/new", (req, res) => {
-  const templateVars = { user: users[req.session.userID], users };
+  const templateVars = { 
+    user: users[req.session.userID]
+   };
   req.session.userID ? res.render('urlsNew', templateVars) : res.redirect('/login');
 });
+
 //Renders edit page to change shortURL
 app.get("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.userID] , urlUserID: urlDatabase[req.params.shortURL].userID };
+    const templateVars = { 
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL].longURL, 
+      user: users[req.session.userID] , 
+      urlUserID: urlDatabase[req.params.shortURL].userID 
+    };
     res.render("urlsShow" , templateVars);
   } else {
     return res.status(404).send("Page not found.")
@@ -81,9 +77,10 @@ app.get("/u/:shortURL", (req, res) => {
 
 //gets 404 page
 app.get("*", (req,res) => {
-  const templateVars = { user: users[req.session.userID] };
-  console.log(req.url);
-  res.status(404) ? res.render('404', templateVars) : res.render('403', templateVars);
+  const templateVars = { 
+    user: users[req.session.userID] 
+  };
+  res.render('404', templateVars);
 });
 
 //updates the users edits to urls
@@ -92,7 +89,7 @@ app.post("/urls/:shortURL", (req,res) => {
     urlDatabase[req.params.shortURL].longURL = req.body.newURL; //updates database
     return res.redirect('/urls');
   } else {
-    res.status(403).send("You are not authorized to remove or edit this url.");
+    return res.status(403).send("You are not authorized to remove or edit this url.");
   }
 });
 
@@ -102,13 +99,15 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     delete urlDatabase[req.params.shortURL];
     return res.redirect('/urls');
   }
-  res.status(403).send("You are not authorized to remove or edit this url.");
+  return res.status(403).send("You are not authorized to remove or edit this url.");
 });
 
 //My URLS with user inputs of URLS
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = { longURL: req.body.longURL, userID: req.session.userID}; //updates database object
+  urlDatabase[shortURL] = { 
+    longURL: req.body.longURL, 
+    userID: req.session.userID}; //updates database object
   return res.redirect(`u/${shortURL}`);
 });
 
@@ -117,7 +116,11 @@ app.post("/register", (req, res) => {
     res.status(404).send("Please ensure that none of the fields were left empty.");
   } else if (!findEmail(req.body.email, users)) { //if email is not found
     const userRandomID = generateRandomString();
-    users[userRandomID] = { id: userRandomID, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10)};
+    users[userRandomID] = { 
+      id: userRandomID, 
+      email: req.body.email, 
+      password: bcrypt.hashSync(req.body.password, 10)
+    };
     req.session.userID = userRandomID; 
     res.redirect('/urls');
   } else {
@@ -126,7 +129,6 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body.password);
   if (req.body.email === '' || req.body.password === '') {
     return res.status(404).send("Please ensure that none of the fields were left empty.");
   } else if (findEmail(req.body.email, users)) { 
--- helper.js
+++ helper.js
@@ -7,4 +7,26 @@ function findEmail(emailInput, users) {
   return undefined;
 }
 
-module.exports = findEmail;
+function generateRandomString() {
+  let alphanumeric = '';
+  for (let i = 0; i < 6; i++) {
+  alphanumeric += Math.random().toString(36).slice(2,3);  
+  }
+  return alphanumeric;
+};
+
+function urlsForUser(id, urlDatabase) {
+  const userBase = {};
+  for(let u in urlDatabase) {
+    if (urlDatabase[u].userID === id) { //if database at the shortURL of the userID key equals id
+      userBase[u] = urlDatabase[u]; //adds key 
+    }
+  }
+  return userBase;
+};
+
+module.exports = {
+  findEmail,
+  generateRandomString,
+  urlsForUser
+}
